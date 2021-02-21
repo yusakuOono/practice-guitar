e50094869d430c99705beae6ff520dc30d3bd063
y.ohno <y.ohno@holyday.co.jp>
2021-02-21 09:15:58 +0900

    firstmoccit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    スケール練習できるよ
+    <FirstView/>
+  </div>
+</template>
+
+<script>
+import FirstView from '@/components/FirstView.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FirstView
+  }
+}
+</script>
+
+<style>
+html{
+  background: #F2EFE4;
+}
+body{
+  margin: 0;
+  height: 100vh;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/FirstView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="NOTE" @click="countUp">
+    <div class="notes">
+      A
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FirstView',
+  data () {
+    return {
+      notes: ['A', 'B♭', 'B', 'C', 'D♭','D', 'E♭', 'E', 'F', 'G♭', 'G', 'A♭' ]
+    }
+  },
+  methods: {
+    countUp() {
+      const number = Math.floor(Math.random() * this.notes.length);
+      const selectedImg = this.notes[number];
+      const note =  document.querySelector(".notes")
+      note.innerHTML = selectedImg;
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .notes{
+    font-size: 140px;
+    cursor: pointer;
+    height: 200px;
+    line-height: 200px;   
+    margin-top: 200px; 
+  }
+</style>
